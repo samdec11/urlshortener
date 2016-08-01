5d217188c5803e4f5e4f9ab5baf033e6c31c4a66
Sean Marzug-McCarthy <samdec11@gmail.com>
2016-08-01 11:57:49 -0400

    a little bit of styling

--- canary/settings.py
+++ canary/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '_vihb-+_*c3-snzv_4&xe^5wbtv5oz)1#_b2)ko&_u#@rncr+i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*',]
+ALLOWED_HOSTS = []
 
 
 # Application definition
